433b2d2fdcea42519193a68d1ad69d5de5a04d1d
Enzo Zamberlan <enzo2017.bra@gmail.com>
2024-11-07 22:32:50 -0300

    .

---
+++ github_finder_ts/src/components/Error.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div>
+      <p>Usuário não encontrado!</p> {/* Colocando o erro hardcoded pois aqui só tem esse tipo de erro, se quiser implementar, é válido. */}
+    </div>
+  );
+};
+
+export default Error;
